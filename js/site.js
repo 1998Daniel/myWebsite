6c8563601f4047c0c15e94c02b8dd4f191657853
Daniel <1998dual@gmail.com>
2019-03-24 21:40:57 -0500

    instantiating

---
+++ js/site.js
@@ -0,0 +1,19 @@
+// Javascript to run the website.
+
+$('a[href^="#"]').on('click', function (event) {
+    event.preventDefault();
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top - 150
+        }, 1000);
+    }
+});
+
+window.onload = function () {
+    $('body').hide();
+    $('body').fadeIn(2000, function () {
+        // Animation complete
+    });
+}
